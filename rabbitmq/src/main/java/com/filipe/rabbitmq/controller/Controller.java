cb62cbf867c8c097bf34bdc8950cc09435ade0a7
filipe <filipe.esteves.goncalves@gmail.com>
2022-06-20 21:04:50 +0100

    Start to use the controller to publish the message

--- rabbitmq/src/main/java/com/filipe/rabbitmq/controller/Controller.java
+++ rabbitmq/src/main/java/com/filipe/rabbitmq/controller/Controller.java
@@ -1,23 +1,37 @@
 package com.filipe.rabbitmq.controller;
 
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.filipe.rabbitmq.RabbitmqApplication;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
-@RequestMapping("ue")
+@RequestMapping()
 public class Controller {
 		
-
-	//public SearchController() {}
+	  private final RabbitTemplate rabbitTemplate;
+	  
+	public Controller(RabbitTemplate rabbitTemplate) {
+	    this.rabbitTemplate = rabbitTemplate;
+	  }
 
 	@GetMapping()
 	public ResponseEntity<?> find() {
-		
+		rabbitTemplate.convertAndSend(RabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+		return ResponseEntity.ok("ok");
+	}
+	
+	@PostMapping()
+	public ResponseEntity<?> create(String body) {
+		rabbitTemplate.convertAndSend(RabbitmqApplication.topicExchangeName, "foo.bar.baz", body);
 		return ResponseEntity.ok("ok");
 	}
 
