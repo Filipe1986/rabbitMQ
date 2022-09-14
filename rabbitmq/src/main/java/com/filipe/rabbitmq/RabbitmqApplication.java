99ba58e1c0ffbb2866f0c47d8886b4e3f88ea8f5
Filipe Gonçalves <festeves@altissia.org>
2022-09-14 09:49:54 +0100

    Change spring parent version, add makefile

--- rabbitmq/src/main/java/com/filipe/rabbitmq/RabbitmqApplication.java
+++ rabbitmq/src/main/java/com/filipe/rabbitmq/RabbitmqApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitmqApplication {
 
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqApplication.class, args);
 	}
