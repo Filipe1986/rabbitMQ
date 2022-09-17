d9de5c02abaa91e83cf8e86d642312ffa7f1e4e1
Filipe Gonçalves <festeves@altissia.org>
2022-09-17 13:36:46 +0100

    Configuring a Dedicated Listener for queue

--- rabbitmqListener/src/main/java/com/filipe/rabbitmq/message/Listener.java
+++ rabbitmqListener/src/main/java/com/filipe/rabbitmq/message/Listener.java
@@ -2,6 +2,7 @@ package com.filipe.rabbitmq.message;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,5 +11,10 @@ public class Listener implements MessageListener {
 	@Override
 	public void onMessage(Message message) {
 		System.out.println(new String(message.getBody()));
+	}
+
+	@RabbitListener(queues = "topicQueue2")
+	public void listen(Message message) {
+		System.out.println(" topicQueue2"  + new String(message.getBody()));
 	}
 }
